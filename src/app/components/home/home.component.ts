670f196e78e3230428b568344c215767acb3aab0
staryag <staryag@gmail.com>
2018-05-31 20:58:36 +0200

    Formulario ejercicios a medias

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,32 +1,18 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  autoTicks = false;
-  disabled = false;
-  invert = false;
-  max = 100;
-  min = 0;
-  showTicks = false;
-  step = 1;
-  thumbLabel = false;
-  value = 0;
-  vertical = false;
-
-  get tickInterval(): number | 'auto' {
-    return this.showTicks ? (this.autoTicks ? 'auto' : this._tickInterval) : 0;
-  }
-  set tickInterval(v) {
-    this._tickInterval = Number(v);
-  }
-  private _tickInterval = 1;
-
+  cardios = [
+    {value: 'comba-0', viewValue: 'Saltos comba'},
+    {value: 'correr-1', viewValue: 'Correr'},
+    {value: 'escaleras-2', viewValue: 'Subir escaleras'}
+  ];
+  
   constructor() { }
 
   ngOnInit() {
