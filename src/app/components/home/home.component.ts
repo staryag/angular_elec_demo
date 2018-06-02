e6a0fa6942c1b50b09ea68f9cceb07143eae5161
Markos <staryag@gmail.com>
2018-06-02 20:33:21 +0200

    Ligeras modificaciones en el diseño del formulario de ejercicios

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,7 +12,19 @@ export class HomeComponent implements OnInit {
     {value: 'correr-1', viewValue: 'Correr'},
     {value: 'escaleras-2', viewValue: 'Subir escaleras'}
   ];
-  
+
+  abdominales = [
+    {value: 'normal-0', viewValue: 'Normales'},
+    {value: 'oblicua-1', viewValue: 'Oblicuas'},
+    {value: 'piernas-2', viewValue: 'Piernas arriba'}
+  ];
+
+  flexiones = [
+    {value: 'normal-0', viewValue: 'Normales'},
+    {value: 'centro-1', viewValue: 'Manos centro'},
+    {value: 'abierta-2', viewValue: 'Manos abiertas'}
+  ];
+
   constructor() { }
 
   ngOnInit() {
